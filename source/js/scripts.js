63178b4cecbf1500df5854df5d3baf95d761dbee
WStalker <zlokazov.k@gmail.com>
2023-11-07 23:10:12 +0300

    Сверстан слайдер. Начало работы над блоком контакты

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,3 +1,5 @@
+// BURGER-MENU
+
 const pageBody = document.querySelector('.page__body');
 const navigation = document.querySelector('.main-navigation');
 const navigationToggle = navigation.querySelector('.main-navigation__toggle');
@@ -13,4 +15,34 @@ navigationToggle.addEventListener('click', (evt) => {
     evt.preventDefault();
     navigation.classList.add('main-navigation--closed');
   }
+});
+
+// SLIDER
+
+const slides = document.querySelectorAll('.slider__item');
+const buttonPrevious = document.querySelector('.slider__button--previous');
+const buttonNext = document.querySelector('.slider__button--next');
+let counter = 0;
+
+const clearClasses = () => {
+  document.querySelector('.slider__item--current').classList.remove('slider__item--current');
+};
+
+buttonPrevious.addEventListener('click', () => {
+  clearClasses();
+  counter = counter - 1;
+  if (counter < 0) {
+    counter = slides.length - 1;
+  }
+
+  slides[counter].classList.add('slider__item--current');
+});
+
+buttonNext.addEventListener('click', () => {
+  clearClasses();
+  counter = counter + 1;
+  if (counter > slides.length - 1) {
+    counter = 0;
+  }
+  slides[counter].classList.add('slider__item--current');
 });
